a8cf4035409b8bb4c3eeffb10401ac5b4f15ee8d
chenxiang <chenxiang@myhexin.com>
2020-05-09 17:31:45 +0800

    ''

--- electra_sg/run_finetuning.py
+++ electra_sg/run_finetuning.py
@@ -98,6 +98,7 @@ def model_fn_builder(config: configure_finetuning.FinetuningConfig, tasks,
             dep_assignment_map, _ = modeling.get_assignment_map_from_checkpoint(
                 tvars, init_checkpoint, prefix="task_specific/squad/dependence/")
             print(dep_assignment_map)
+            print(tf.trainable_variables())
             if config.use_tpu:
                 def tpu_scaffold():
                     tf.train.init_from_checkpoint(init_checkpoint, assignment_map)
