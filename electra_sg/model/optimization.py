38a4205e8af0f9e839eeb6f82e8d1436aa2d07ce
chenxiang <chenxiang@myhexin.com>
2020-05-11 11:35:15 +0800

    ''

--- electra_sg/model/optimization.py
+++ electra_sg/model/optimization.py
@@ -133,8 +133,8 @@ class AdamWeightDecayOptimizer(tf.train.Optimizer):
             lr_weight = 1.0
             if "/dependence/" in param_name:
                 print(param_name)
-                lr_weight = 4.0
-            update_with_lr = learning_rate * update
+                lr_weight = 2.0
+            update_with_lr = learning_rate * update * lr_weight
             next_param = param - update_with_lr
 
             assignments.extend(
