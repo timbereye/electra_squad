8a00e9106b92ccc298bcd887965f7edd54b81a3d
chenxiang <chenxiang@myhexin.com>
2020-06-15 17:02:04 +0800

    test

--- electra_sg/model/modeling.py
+++ electra_sg/model/modeling.py
@@ -376,7 +376,7 @@ def get_var_map_from_checkpoint(tvars, init_checkpoint, prefix=""):
         value = tf.train.load_variable(init_checkpoint, name)
         if prefix + name not in name_to_variable:
             continue
-        var_map[prefix + name] = tf.constant(value, var)
+        var_map[prefix + name] = tf.constant(value, shape=var)
 
     return var_map
 
